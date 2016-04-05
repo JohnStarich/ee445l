778c5b75c4c7f2cf001599526ba7dc382c8926ec
John Starich <johnstarich@johnstarich.com>
2016-04-05 12:50:26 -0500

    Fix compilation of Lab 8

--- Lab8/Buttons.c
+++ Lab8/Buttons.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include "Buttons.h"
 #include "Timer1A.h"
-#include "Music.h"
 
 void Buttons_Init(void) {
 	SYSCTL_RCGCGPIO_R |= 0x10;        // 1) activate clock for Port E
@@ -19,7 +18,7 @@ void Buttons_Init(void) {
 	
 	Timer1_Init();
 }
-// bool debounce[4] = { hours, minutes, arm_alarm, set_alarm };
+
 uint16_t debounce[4];
 
 #define CYCLE_WAIT 5
@@ -36,31 +35,17 @@ void Buttons_ReadInput(void) {
 		debounce[3] = ((data & 0x8) >> 3)*CYCLE_WAIT;
 }
 
-extern bool Play_mode;
-void Pause(void);
-void Play(void);
-void Rewind(void);
-
-
 void Buttons_Pressed(uint32_t button) {
 	if(button == 0) {											// play/pause button
-		if(Play_mode) {
-			Pause();
-		}
-		else {
-			Play();
-		}
 	}
 	else if(button == 1) {								// rewind button
-		Rewind();
 	}
 	else if (button == 2) {
-		TIMER0_TAILR_R = F64HZ-1;
 	}
 	else if (button == 3) {
-		TIMER0_TAILR_R = F16HZ-1;
 	}
 }
+
 void Buttons_10ms_Handler(void) {
 	Buttons_ReadInput();
 	for(uint32_t i = 0; i < 4; i += 1) {
@@ -71,25 +56,4 @@ void Buttons_10ms_Handler(void) {
 			}
 		}
 	}
-}
-
-void Play(void) {
-	Play_mode = true;
-	NVIC_ST_CTRL_R = 0x07;
-	TIMER0_CTL_R = 0x00000001;
-	NVIC_ST_RELOAD_R = 500;
-}
-
-void Pause(void) {
-	Play_mode = false;
-	NVIC_ST_CTRL_R = 0;
-	TIMER0_CTL_R = 0x00000000;
-}
-
-extern const Song song;
-extern const Song Prelude;
-
-void Rewind(void) {
-	Pause();
-	Song_PlayInit(Prelude);
 }
